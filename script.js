e240ec5da72c4fc8c9285404aeab2cb8bdc253ec
marvieiradev <marcosvieira482@gmail.com>
2024-10-04 22:54:13 -0300

    add script.js

---
+++ script.js
@@ -0,0 +1,39 @@
+let images = [
+  "🍇",
+  "🍉",
+  "🍊",
+  "🍋",
+  "🍍",
+  "🥭",
+  "🍎",
+  "🍑",
+  "🍒",
+  "🍓",
+];
+
+document.addEventListener("mousemove", (e) => {
+  let body = document.querySelector("body");
+  let emoji = document.createElement("span");
+  let x = e.offsetX;
+  let y = e.offsetY;
+
+  emoji.style.left = x + "px";
+  emoji.style.top = y + "px";
+
+  let icon = images[Math.floor(Math.random() * images.length)];
+  emoji.innerText = icon;
+
+  let size = Math.random() * 50;
+  emoji.style.fontSize = 5 + size + "px";
+
+  let max = 0;
+  let min = 200;
+  let randomValue = Math.floor((Math.random() * ((max + 1)) - min) + min);
+
+  emoji.style.transform = `translateX(${randomValue}px)`;
+  body.appendChild(emoji);
+
+  setTimeout(()=>{
+    emoji.remove();
+  },1000);
+});
